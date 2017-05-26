356283acafc9a8d410e7d23e585de9b9c28a0d79
Anthonie Lance <anthonielance@users.noreply.github.com>
2017-05-26 10:42:08 -0400

    Include XML documentation

--- All.AspNetCore.Routing/Constraints/SimpleEmailRouteConstraint.cs
+++ All.AspNetCore.Routing/Constraints/SimpleEmailRouteConstraint.cs
@@ -2,8 +2,14 @@
 
 namespace All.AspNetCore.Routing.Constraints
 {
+    /// <summary>
+    /// Constrains a route parameter that must contain at least one "@" symbol.
+    /// </summary>
     public class SimpleEmailRouteConstraint : RegexRouteConstraint
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleEmailRouteConstraint"/> class.
+        /// </summary>
         public SimpleEmailRouteConstraint() : base("@")
         {            
         }
